4cd5961e76c3d49a7064afee2a8e3e01d3a5933a
Noam Ram <noamramny@gmail.com>
2024-05-11 11:43:08 +0300

    added C# code

---
+++ SectionThree/ThirdSection/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace MyApp
+{
+    class Node
+    {
+        private int value;
+        private Node? next;
+
+        public Node(int value, Node next)
+        {
+            this.value = value;
+            this.next = next;
+        }
+        public Node(int value)
+        {
+            this.value = value;
+        }
+        public Node() { }
+
+        public int GetValue()
+        {
+            return value;
+        }
+        public Node GetNext()
+        {
+            return next!;
+        }
+        public void SetNext(Node next)
+        {
+            this.next = next;
+        }
+        public void SetValue(int val)
+        {
+            this.value = val;
+        }
+    }
+}
+
---
+++ SectionThree/ThirdSection/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellorld!");
+
+            NumericalExpression NumEx = new NumericalExpression(53211857887609);
+            Console.WriteLine(NumEx);
+
+
+
+
+            //LinkedList list = new LinkedList(2);
+            //list.Prepend(10);
+            //list.Append(2222);
+            //list.Prepend(15);
+            //list.Append(3333);
+            //list.Append(332);
+
+            //foreach (int i in list.ToList())
+            //{
+            //    Console.WriteLine(i.ToString());
+            //}
+
+            //Console.WriteLine("--------------------");
+
+
+            //Node max = list.GetMaxNode();
+            //Console.WriteLine("Max node: " + max.GetValue());
+
+            //Node min = list.GetMinNode();
+            //Console.WriteLine("Min node: " + min.GetValue());
+
+            //Console.WriteLine("SORTING: ");
+            //list.Sort();
+            //foreach (int i in list.ToList())
+            //{
+            //    Console.Write(i + ", ");
+            //}
+            //Console.WriteLine();
+            //max = list.GetMaxNode();
+            //Console.WriteLine("Max node: " + max.GetValue());
+
+            //min = list.GetMinNode();
+            //Console.WriteLine("Min node: " + min.GetValue());
+        }
+    }
+}
